338b5929a942295402b9edc75602ff99586a657e
Leonardo Castro <twelve.programming@gmail.com>
2024-07-18 16:59:03 -0400

    Adicionando a função system

--- aulas/hello.c
+++ aulas/hello.c
@@ -2,5 +2,8 @@
 
 int main(){
     printf("Hello World!\n"); //Primeiro código e comentário
+
+    system("pause"); //função para parar a execução(útil quando for abrir diretamente o arquivo .exe)
+
     return 0;
 }
