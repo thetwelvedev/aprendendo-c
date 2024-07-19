eab2a404bcb7aef246d46e95fbdae424b54edd9a
Leonardo Castro <twelve.programming@gmail.com>
2024-07-19 19:00:44 -0400

    Testei o debug do vs code e modifiquei uns detalhes

--- aulas/debugando.c
+++ aulas/debugando.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(){ //int argc, char *argv[] -  tirei o argumento para não aparecer "unused parameter 'argc' [-Wunused-argument]"
     
     printf("Mensagem 1.\n");
     printf("Mensagem 2.\n");
     printf("Mensagem 3.\n");
     printf("Mensagem 4.\n");
-    printf("Mensagem 5.\n");	
+    printf("Mensagem 5.\n");
     return 0;
 }
