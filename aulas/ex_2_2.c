5cb6d2a514afb60b25befde9913311753a0011fc
Leonardo Castro <twelve.programming@gmail.com>
2024-07-21 18:02:14 -0400

    Exemplo final do uso do scanf

--- aulas/ex_2_2.c
+++ aulas/ex_2_2.c
@@ -2,13 +2,16 @@
 
 int main(){
     int idade = 0;
-
-    printf("Valor inicial da idade: %d.\n", idade);
+    float peso = 0.0;
 
     printf("Digite uma idade:\n");
     scanf("%d", &idade);
 
+    printf("Digite um peso:\n");
+    scanf("%f", &peso);
+
     printf("Idade informada: %d.\n", idade);
+    printf("peso informada: %.2f.\n", peso);
 
     return 0;
 }
