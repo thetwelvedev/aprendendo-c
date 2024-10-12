c289f0a9b36994b176f7329641842ebf18180558
Leonardo Castro <twelve.programming@gmail.com>
2024-10-12 00:02:41 -0400

    Aula sobre laços de repetição

--- curso-c-udemy/aulas/programa2.c
+++ curso-c-udemy/aulas/programa2.c
@@ -1,9 +1,27 @@
-//
+//Estruturas de decisão if, else, else if
 #include <stdio.h>
 #include <locale.h>
 
 int main(){
 setlocale(LC_ALL, "portuguese");
+	//Declaração de variáveis
+	int idade;
+
+	//Entrada
+	printf("Qual é a idade? ");
+	scanf("%d", &idade);
+
+	//Processamento
+	if(idade < 18){
+		printf("Você é menor de idade.\n");
+	}else if(idade >= 18 && idade < 60){
+		printf("Você é adulto.\n");
+	}else{
+		printf("Você é idoso.\n");
+	}
+
+	//Saída
+	printf("Sua idade é %d", idade);
 	
 	return 0;
 }
