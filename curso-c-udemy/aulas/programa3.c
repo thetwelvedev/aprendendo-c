be6a8eb0a543c38ecb02e450691f46e8b8a18445
Leonardo Castro <twelve.programming@gmail.com>
2024-10-14 23:30:23 -0400

    Uso do For

--- curso-c-udemy/aulas/programa3.c
+++ curso-c-udemy/aulas/programa3.c
@@ -4,6 +4,17 @@
 
 int main(){
 setlocale(LC_ALL, "portuguese"); 
+	int num, soma = 0;
+
+    // Loop para receber 5 números inteiros
+    for (int i = 0; i < 5; i++) {
+        printf("Digite um número inteiro: ");
+        scanf("%d", &num);
+        soma += num;  // Soma o número inserido à variável soma
+    }
+
+    // Exibe a soma dos números
+    printf("A soma dos 5 números é: %d\n", soma);
 	
 	return 0;
 }
