c322e31720b7fb2cf5eded0327e7a4ac298d7127
Leonardo Castro <twelve.programming@gmail.com>
2024-10-15 20:04:56 -0400

    Apredendo sobre os tipos booleando

---
+++ curso-c-udemy/aulas/programa10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <locale.h>
+//Tipos de dados
+
+//Booleanos (Verdadeiro/Falso) true/false
+
+/*
+Na linguagem C, não existe um tipo de dado boolean
+
+Mas...
+
+A linguagem C, reconhece o valor 0 como Falso (false)
+e Qualquer valor diferente de 0 como Verdadeiro (true)
+*/
+int main(){
+	setlocale(LC_ALL, "portuguese"); 
+	int booleano = 1;
+
+	if(0){
+		printf("Verdadeiro...");
+	}else{
+		printf("Falso...");
+	}
+
+	return 0;
+}
