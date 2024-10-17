b081b73c694fbae04d5b082190f44936f0485788
Leonardo Castro <twelve.programming@gmail.com>
2024-10-17 00:04:59 -0400

    Exercícios da seção 7

---
+++ curso-c-udemy/exercícios/exercício_seção_6/ex002.c
@@ -0,0 +1,9 @@
+//Faça um programa que leia um vetor de 10 posições. Conte e mostre quantos valores pares ele possui.
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+setlocale(LC_ALL, "portuguese");
+
+	return 0;
+}
---
+++ curso-c-udemy/exercícios/exercício_seção_6/ex003.c
@@ -0,0 +1,9 @@
+//Faça um programa que leia um vetor de 10 posições e atribua valor 0 para todos os elementos que possuírem valores negativos.
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+setlocale(LC_ALL, "portuguese");
+
+	return 0;
+}
