4c71807ac8cd7a66b1d98ed04212097813bd9ec8
Leonardo Castro <twelve.programming@gmail.com>
2024-10-16 23:06:03 -0400

    Uso do velores com string e char

--- curso-c-udemy/aulas/programa12.c
+++ curso-c-udemy/aulas/programa12.c
@@ -4,6 +4,28 @@
 
 int main(){
 setlocale(LC_ALL, "portuguese");
+	//Vetores e strings
+	char nome[50];
+	printf("Qual o seu nome? ");
+	gets(nome);
+	printf("Olá %s\n", nome);
+
+	//Vetores e caracteres
+	char letras[26];
+	int contador = 0;
+	for(int i = 97; i <= 122; i++){
+		letras[contador] = i;
+		contador += 1;
+	}
+	//Imprimindo as letras e seus valores em decimal
+	for(int i = 0; i <= 25; i++){
+		printf("%d == %c\n", letras[i], letras[i]);
+	}
+	// //Vetores de inteiros
+	// int numeros[10];
+
+	// //Vetores e reais
+	// float valores[5];
 
 	return 0;
 }
