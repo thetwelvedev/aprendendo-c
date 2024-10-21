ce857e6aed7cfd99a995c92e213e0129f41fdcc5
Leonardo Castro <twelve.programming@gmail.com>
2024-10-21 15:59:44 -0400

    Aprendendo a declarar, receber o endereço de mémoria e com o próprio ponteiro ter  saída do endereço que ele aponta e o valor

--- curso-c-udemy/aulas/programa22.c
+++ curso-c-udemy/aulas/programa22.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main(){
-	int n; // variÃ¡vel que guarda seu valor na memÃ³ria
+	setlocale(LC_ALL, "portuguese"); 
+	int n; // variável que guarda seu valor na memória
 
 	//declarando
-	int* p; //ponteiro Ã© um valor que aponta para um endereÃ§o de memÃ³ria
+	int* p; //ponteiro é um valor que aponta para um endereço de memória
 
-	printf("Informe um nÃºmero: ");
+	printf("Informe um número: ");
 	scanf("%d", &n);
 
 	//inicializando o ponteiro
-	p = &n;
+	p = &n; //recebendo o endereço de mémoria
 
-	printf("O nÃºmero informado foi: %d\n", n);
+	printf("O número informado foi: %d\n", n);
 
-	printf("EndereÃ§o de memÃ³ria: %p\n", &n);
+	printf("Endereço de memória: %p\n", &n);
 
-	printf("EndereÃ§o do ponteiro: %p\n", p);
+	printf("Endereço do ponteiro: %p\n", p);
+
+	printf("O valor que está armazenado no endereço de mémoria que o ponteiro p está apontando é: %d\n", (*p));
 
 	return 0;
 }
