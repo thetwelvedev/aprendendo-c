b0d5bec85f0c302dab14b22595d83871652341e1
Leonardo Castro <twelve.programming@gmail.com>
2024-11-10 15:58:25 -0400

    Criação da função jachutou

--- curso-c-alura/2-C-Avançado-na-linguagem/forca.c
+++ curso-c-alura/2-C-Avançado-na-linguagem/forca.c
@@ -17,6 +17,19 @@ void chuta(char chutes[26], int* tentativas){ //Esse nome tentativas é só o nome
         (*tentativas)++; //Dessa forma o ++ vai ser no conteúdo e não no endereço de memória
 }
 
+int jachutou(char letra, char chutes[26], int tentativas){
+    int achou = 0;
+    //O loop principal vai rodar esse segundo loop inteiro a cada interação com isso ele faz a comparação de todas as letras de chutes(até o momento) com cada posição em palavra secreta.
+    for(int j = 0; j < tentativas; j++) { //Vai rodar a partir do momento que tentativas = 1 
+        if(chutes[j] == letra) { //Aqui a letra de chute vai comparar com a letra de palavra secreta, se tiver 3 letras em chutes, então seram feita 3 comparações(se acertar para a comparação)
+            achou = 1;
+            break;
+        }
+    }
+
+    return achou;
+}
+
 int main() {
     setlocale(LC_ALL, "Portuguese");
     char palavrasecreta[20];
@@ -34,14 +47,9 @@ int main() {
     do {
         //Todo novo loop o for começa do 0 por isso ele compara todas as letras
         for(int i = 0; i < strlen(palavrasecreta); i++) { // Como não tem um número de tentativas no primeiro loop ele vai imprimir logo a forca vazia, todo loop desse as letras de chutes compararam com todas as letras de palavra secreta
-            int achou = 0;
-            //O loop principal vai rodar esse segundo loop inteiro a cada interação com isso ele faz a comparação de todas as letras de chutes(até o momento) com cada posição em palavra secreta.
-            for(int j = 0; j < tentativas; j++) { //Vai rodar a partir do momento que tentativas = 1 
-                if(chutes[j] == palavrasecreta[i]) { //Aqui a letra de chute vai comparar com a letra de palavra secreta, se tiver 3 letras em chutes, então seram feita 3 comparações(se acertar para a comparação)
-                    achou = 1;
-                    break;
-                }
-            }
+
+            //Aqui está o código
+            int achou = jachutou(palavrasecreta[i], chutes, i++);
 
             if(achou) {
                 printf("%c ", palavrasecreta[i]); //Quando acha coloca a letra no lugar do traço
