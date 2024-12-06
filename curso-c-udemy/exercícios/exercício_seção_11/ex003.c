3a846b3e5641d79d4a1cfb9676ae45074702b54c
Leonardo Castro <twelve.programming@gmail.com>
2024-12-06 13:55:31 -0400

    Versão final do exercício 3 de struct

--- curso-c-udemy/exercícios/exercício_seção_11/ex003.c
+++ curso-c-udemy/exercícios/exercício_seção_11/ex003.c
@@ -3,25 +3,28 @@ Implemente um programa que receba, calcule e apresente a soma de dois vetores.*/
 #include <stdio.h>
 #include <string.h>
 
-typedef struct vetor{
+typedef struct{
     float x;
     float y;
     float z;
 }Vetor;
 
-int main(){
-    Vetor v1, v2, soma;
+#include <stdio.h>
 
-    printf("Digite a coordenada do vetor 1 (x,y,z): ");
-    scanf("%f %f %f", &v1.x, &v1.y, &v1.z);
-    printf("Digite a coordenada do vetor 2 (x,y,z): ");
-    scanf("%f %f %f", &v2.x, &v2.y, &v2.z);
-    //Soma de vetores
-    soma.x = v1.x + v2.x;
-    soma.y = v1.y + v2.y;
-    soma.z = v1.z + v2.z;
+int main() {
+    Vetor vetores[3]; 
     
-    printf("Vetor resultante da soma: (%.2f, %.2f, %.2f)", soma.x, soma.y, soma.z);
+    for (int i = 0; i < 2; i++) {
+        printf("Digite a coordenada do vetor %d (x, y, z): ", i + 1);
+        scanf("%f %f %f", &vetores[i].x, &vetores[i].y, &vetores[i].z);
+    }
+    
+    vetores[2].x = vetores[0].x + vetores[1].x;
+    vetores[2].y = vetores[0].y + vetores[1].y;
+    vetores[2].z = vetores[0].z + vetores[1].z;
+
+    printf("Vetor resultante da soma: (%.2f, %.2f, %.2f)\n", vetores[2].x, vetores[2].y, vetores[2].z);
 
     return 0;
-} 
+}
+ 
