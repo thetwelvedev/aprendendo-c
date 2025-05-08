d26f860415de397f0f467d2162b18894c5434316
Leonardo Castro <twelve.programming@gmail.com>
2025-05-08 11:54:17 -0400

    Recebendo valores do usuário

--- revisão/ProgramaComandosC.c
+++ revisão/ProgramaComandosC.c
@@ -9,6 +9,8 @@ int main(void){
     char frase[10] = "Bom dia!"; //string
     double n3 = 1.23456; //variável real com precisão dupla
 
+    int valor1, valor2, soma;
+
     //Usos do comando printf com diversas variáveis
     printf("Hello World!\n");
     printf("Exibindo o numero inteiro(integer) %d\n", n);
@@ -17,6 +19,13 @@ int main(void){
     printf("Exibindo uma string(char) %s\n", frase);
     printf("Exibindo o numero real de dupla precisao(double) %f\n", n3);
     printf("Valores: %d %f %c %s %f\n", n, n2, letra, frase, n3); //Exibindo várias variáveis num mesmo print
+
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &valor1);
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &valor2);
+
+    printf("Valor da soma de %d + %d = %d\n", valor1, valor2, soma);
 
     system("Pause"); //somente para windows
     return 0;
