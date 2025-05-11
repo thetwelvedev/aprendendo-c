d45b144e25f6fe55a93b7fadd5704432cc6a8158
Leonardo Castro <twelve.programming@gmail.com>
2025-05-11 15:37:34 -0400

    Aprendendo sobre os operadores aritméticos

--- revisão/ProgramaComandosC.c
+++ revisão/ProgramaComandosC.c
@@ -9,7 +9,7 @@ int main(void){
     char frase[10] = "Bom dia!"; //string
     double n3 = 1.23456; //variável real com precisão dupla
 
-    int valor1, valor2, soma;
+    int valor1, valor2, soma, sub, mult, div;
 
     //Usos do comando printf com diversas variáveis
     printf("Hello World!\n");
@@ -20,12 +20,22 @@ int main(void){
     printf("Exibindo o numero real de dupla precisao(double) %f\n", n3);
     printf("Valores: %d %f %c %s %f\n", n, n2, letra, frase, n3); //Exibindo várias variáveis num mesmo print
 
+    //Utilização de scanf
     printf("Digite um valor inteiro: ");
     scanf("%d", &valor1);
     printf("Digite um valor inteiro: ");
     scanf("%d", &valor2);
 
+    //Operadores aritméticos
+    soma = valor1 + valor2;
+    sub = valor1 - valor2;
+    mult = valor1 * valor2;
+    div = valor1 / valor2;
+
     printf("Valor da soma de %d + %d = %d\n", valor1, valor2, soma);
+    printf("Valor da subtracao de %d - %d = %d\n", valor1, valor2, sub);
+    printf("Valor da multiplicacao de %d * %d = %d\n", valor1, valor2, mult);
+    printf("Valor da divisao de %d / %d = %d\n", valor1, valor2, div);
 
     system("Pause"); //somente para windows
     return 0;
