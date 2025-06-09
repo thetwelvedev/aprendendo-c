dbfcb4d20404ceaea226b5abf449f6107a561ece
Leonardo Castro <twelve.programming@gmail.com>
2025-06-08 22:31:09 -0400

    Atividade 2 sobre arquivos feita

--- curso-c-udemy/exercícios/exercício_seção_10/ex002.c
+++ curso-c-udemy/exercícios/exercício_seção_10/ex002.c
@@ -1,10 +1,28 @@
 /*2. Faça um programa que receba do usuário o nome de um arquivo texto e mostre na tela quantas linhas 
 este arquivo possui.*/
 #include <stdio.h>
-#include <locale.h>
 
-int main(){
-setlocale(LC_ALL, "portuguese");
+int main() {
+    FILE *arq;
+    char arquivo[100], c;
+    int linhas = 0;
 
-	return 0;
+    printf("Digite o nome do arquivo: \n");
+    scanf("%s", arquivo);
+
+    arq = fopen(arquivo, "r");
+    if (arq) {
+        while ((c = fgetc(arq)) != EOF) {
+            if (c == '\n') {
+                linhas++;
+            }
+        }
+        fclose(arq);
+
+        printf("O arquivo possui %d linha(s).\n", linhas);
+    } else {
+        printf("Nao foi possivel abrir o arquivo para leitura.\n");
+    }
+
+    return 0;
 }
