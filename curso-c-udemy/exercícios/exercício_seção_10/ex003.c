9baf94faa65f6c5ed95e0ae65d4a9711ceaff6f6
Leonardo Castro <twelve.programming@gmail.com>
2025-06-08 22:37:37 -0400

    Atividade 3 de arquivos feita

--- curso-c-udemy/exercícios/exercício_seção_10/ex003.c
+++ curso-c-udemy/exercícios/exercício_seção_10/ex003.c
@@ -1,10 +1,31 @@
 /*3. Faça um programa que receba do usuário o nome de um arquivo texto e um caractere. Mostre na tela 
 quantas vezes aquele caractere aparece dentro do arquivo.*/
 #include <stdio.h>
-#include <locale.h>
 
-int main(){
-setlocale(LC_ALL, "portuguese");
+int main() {
+    FILE *arq;
+    char arquivo[100], c, caractere;
+    int contador = 0;
 
-	return 0;
+    printf("Digite o nome do arquivo: \n");
+    scanf("%s", arquivo);
+
+    printf("Digite o caractere a ser contado: \n");
+    scanf(" %c", &caractere); // espaço antes de %c para ignorar '\n' pendente
+
+    arq = fopen(arquivo, "r");
+    if (arq) {
+        while ((c = fgetc(arq)) != EOF) {
+            if (c == caractere) {
+                contador++;
+            }
+        }
+        fclose(arq);
+
+        printf("O caractere '%c' aparece %d vez(es) no arquivo.\n", caractere, contador);
+    } else {
+        printf("Nao foi possivel abrir o arquivo para leitura.\n");
+    }
+
+    return 0;
 }
