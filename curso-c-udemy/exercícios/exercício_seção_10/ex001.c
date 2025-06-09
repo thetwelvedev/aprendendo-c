efdd902902424b9706e08759e9556fa8ce626142
Leonardo Castro <twelve.programming@gmail.com>
2025-06-08 22:18:01 -0400

    Atividade 1 sobre arquivos feita

--- curso-c-udemy/exercícios/exercício_seção_10/ex001.c
+++ curso-c-udemy/exercícios/exercício_seção_10/ex001.c
@@ -8,7 +8,37 @@ d) Abra e leia o arquivo, caractere por caractere, e escreva na tela todos os ca
 #include <stdlib.h>
 
 int main() {
-    
+    FILE *arq;
+    char nome[10];
 
-	return 0;
+    arq = fopen("arq.txt", "w");
+    if (arq) {
+        printf("Escreva nomes de pessoas e, quando quiser parar, digite 0:\n");
+        fgets(nome, 10, stdin);
+
+        while (nome[0] != '0') {
+            fputs(nome, arq);
+            printf("Escreva nomes de pessoas e, quando quiser parar, digite 0:\n");
+            fgets(nome, 10, stdin);
+        }
+		fclose(arq);
+
+    } else {
+        printf("Não foi possível abrir o arquivo para escrita.\n");
+    
+    }
+
+    arq = fopen("arq.txt", "r");
+    if (arq) {
+        char ch;
+        printf("\nConteudo do arquivo:\n");
+        while ((ch = fgetc(arq)) != EOF) {
+            printf("%c", ch);
+        }
+        fclose(arq);
+    } else {
+        printf("Não foi possível abrir o arquivo para leitura.\n");
+    }
+
+    return 0;
 }
