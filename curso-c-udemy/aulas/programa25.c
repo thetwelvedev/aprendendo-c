0d33cb33b5601a21809c2b2ebb6efee8560513f2
Leonardo Castro <twelve.programming@gmail.com>
2025-06-08 20:40:58 -0400

    Aprendendo a fazer a leitura do arquivo

--- curso-c-udemy/aulas/programa25.c
+++ curso-c-udemy/aulas/programa25.c
@@ -3,14 +3,21 @@
 
 int main(){
 	FILE *arq;
+	char c;
 
 	//fopen(nome-do-arquivo, forma-de-abertura-do-arquivo)
 	//w - abrir o arquivo para escrita (se o arquivo já existir, será sobrescrito com um novo zerado)
 	//r - abrir o arquivo para leitura (não podemos escrever no arquivo)
 	//wa - abrir o arquivo para adição de conteúdo (se o arquivo já existir, o conteúdo será adicionado nas linhas abaixo)
-	arq = fopen("arquivo.txt", "w");
+	arq = fopen("arquivo.txt", "r");
 
-
+	if(arq){
+		while ((c = getc(arq)) != EOF){// End of File
+			printf("%c", c);
+		}
+	} else{
+		printf("Nao achei o arquivo");
+	}
 	//sempre que a gente finalizar a manipulação de um arquivo, devemos fechá-lo
 	fclose(arq);
 
