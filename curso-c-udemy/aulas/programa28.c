17db72fa69352c8a6c220fae5dd5361a375e4581
Leonardo Castro <twelve.programming@gmail.com>
2025-06-08 21:25:16 -0400

    Aprendendo sobre escrita de arquivo em c

---
+++ curso-c-udemy/aulas/programa28.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	FILE *arq;
+    char fruta[10];
+
+	arq = fopen("frutas.txt", "w");
+
+    if(arq){
+        printf("Informe uma fruta, ou 0 para sair: \n");
+        fgets(fruta, 10, stdin); //stdin = standard input -> entrada padrão
+        while (fruta[0] != '0'){
+            fputs(fruta, arq);
+            printf("Informe uma fruta, ou 0 para sair: \n");
+            fgets(fruta, 10, stdin); 
+        }
+        
+    }else{
+        printf("Nao foi possivel criar o arquivo.");
+    }
+
+
+	fclose(arq);
+	return 0;
+}
