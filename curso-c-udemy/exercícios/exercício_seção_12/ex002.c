65caed64165c13421c18117dd3faebc5029fac4d
Leonardo Castro <twelve.programming@gmail.com>
2025-06-10 23:50:10 -0400

    Adicionei o caso para números negativos

--- curso-c-udemy/exercícios/exercício_seção_12/ex002.c
+++ curso-c-udemy/exercícios/exercício_seção_12/ex002.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 
 int fatorial(int n){
+    if (n < 0) {
+        printf("Erro: Número negativo!\n");
+        return -1; 
+    }
+
     if(n == 1 || n == 0){
         return 1;
     }
