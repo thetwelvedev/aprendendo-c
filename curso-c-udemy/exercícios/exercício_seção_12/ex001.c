6bafd638cb00c9a18dbe7faeb10ef51f89a91a61
Leonardo Castro <twelve.programming@gmail.com>
2025-06-10 23:42:34 -0400

    Adicionando as condições de parada parea inteiros negativos

--- curso-c-udemy/exercícios/exercício_seção_12/ex001.c
+++ curso-c-udemy/exercícios/exercício_seção_12/ex001.c
@@ -3,6 +3,10 @@ apresente o somatório dos números de 1 a N. */
 #include <stdio.h>
 
 int somatorio(int n){
+    if(n < 0){
+        printf("Valor tem que ser positivo!.\n");
+        return 0;
+    }
     if(n <= 1){ //Caso base
         return n;
     }
