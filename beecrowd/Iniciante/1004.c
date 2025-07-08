6db42ad3c6b0ef643f7569c67aff52cf0e371d97
Leonardo Castro <twelve.programming@gmail.com>
2025-07-07 23:08:37 -0400

    Problema Beecrowd 1004

--- beecrowd/Iniciante/1004.c
+++ beecrowd/Iniciante/1004.c
@@ -1,3 +1,5 @@
+/*Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. 
+A seguir mostre a variável PROD com mensagem correspondente.   */
 #include <stdio.h>
  
 int main() {
