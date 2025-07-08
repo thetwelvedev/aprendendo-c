3f644c345e472e5537b7307874437dd3affb78f5
Leonardo Castro <twelve.programming@gmail.com>
2025-07-07 23:07:58 -0400

    Problema Beecrowd 1003

--- beecrowd/Iniciante/1003.c
+++ beecrowd/Iniciante/1003.c
@@ -1,3 +1,5 @@
+/*Leia dois valores inteiros, no caso para variáveis A e B. 
+A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.*/
 #include <stdio.h>
  
 int main() {
