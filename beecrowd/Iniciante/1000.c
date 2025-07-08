18e20f80e8c4210f68843d47aede9a23ecfc98f5
Leonardo Castro <twelve.programming@gmail.com>
2025-07-07 23:04:40 -0400

    Problema Beecrowd 1000

--- beecrowd/Iniciante/1000.c
+++ beecrowd/Iniciante/1000.c
@@ -1,3 +1,5 @@
+/*O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". 
+Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.*/
 #include <stdio.h>
  
 int main() {
