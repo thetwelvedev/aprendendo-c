29aae55e959a4b34c99a3fb376d9094f89c4b08e
Leonardo Castro <twelve.programming@gmail.com>
2025-07-07 23:07:06 -0400

    Problema Beecrowd 1002

--- beecrowd/Iniciante/1002.c
+++ beecrowd/Iniciante/1002.c
@@ -1,3 +1,5 @@
+/*A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.*/
 #include <stdio.h>
 #include <math.h>
 
