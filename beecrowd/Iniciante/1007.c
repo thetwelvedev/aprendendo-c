6375fc9a245b87835b7e624c60d30cfe8ba89dc5
Leonardo Castro <twelve.programming@gmail.com>
2025-07-07 23:13:04 -0400

    Problema Beecrowd 1007

--- beecrowd/Iniciante/1007.c
+++ beecrowd/Iniciante/1007.c
@@ -1,4 +1,5 @@
-/**/
+/*Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: 
+DIFERENCA = (A * B - C * D).*/
 #include <stdio.h>
  
 int main() {
