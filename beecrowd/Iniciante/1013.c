9cb4a136904ab95b0798be12b9b5b6f24d8bbb1c
Leonardo Castro <twelve.programming@gmail.com>
2025-07-07 23:40:30 -0400

    Problema Beecrowd 1013

--- beecrowd/Iniciante/1013.c
+++ beecrowd/Iniciante/1013.c
@@ -1,6 +1,5 @@
 /*Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. 
-Utilize a fórmula:
-
+Utilize a fórmula: MaiorAB = (a+b+abs+(a - b))/ 2
 Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.*/
 #include <stdio.h>
  
