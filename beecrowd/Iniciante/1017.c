3f2f3d36b8922fa8687a539f76756c6d6c6ab675
Leonardo Castro <twelve.programming@gmail.com>
2025-07-28 22:00:29 -0400

    Código 1017

--- beecrowd/Iniciante/1017.c
+++ beecrowd/Iniciante/1017.c
@@ -1,6 +1,20 @@
+/*Joaozinho quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar um automóvel que faz 12 KM/L. 
+Para isso, ele gostaria que você o auxiliasse através de um simples programa. Para efetuar o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) 
+e a velocidade média durante a mesma (em km/h). Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam necessários. 
+Mostre o valor com 3 casas decimais após o ponto.*/
 #include <stdio.h>
  
 int main() {
-    
+    int tempo, velocidade;
+    float distancia, litros;
+
+    scanf("%d %d", &tempo, &velocidade);
+
+    distancia = tempo * velocidade;
+
+    litros = distancia / 12.0;
+
+    printf("%.3f\n", litros);
+
     return 0;
 }
