9f721097b283c82db29c28fd5d8ca12c06311953
Leonardo Castro <twelve.programming@gmail.com>
2025-08-03 23:45:06 -0400

    Código 1019

--- beecrowd/Iniciante/1019.c
+++ beecrowd/Iniciante/1019.c
@@ -1,6 +1,19 @@
+/*Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato horas:minutos:segundos.*/
 #include <stdio.h>
- 
+
 int main() {
+    int N; 
+    int horas, minutos, segundos;
+
+    scanf("%d", &N);
     
+    horas = N / 3600;
+    N = N % 3600;
+
+    minutos = N / 60;
+    segundos = N % 60;
+
+    printf("%d:%d:%d\n", horas, minutos, segundos);
+
     return 0;
 }
