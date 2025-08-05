4a16a30f37030cdfd92ea9bb03c0a730d7fa52a2
Leonardo Castro <twelve.programming@gmail.com>
2025-08-04 23:06:00 -0400

    Código 2020

--- beecrowd/Iniciante/1020.c
+++ beecrowd/Iniciante/1020.c
@@ -1,6 +1,24 @@
+/*Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
+Obs.: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. 
+Nos casos de teste nunca haverá uma situação que permite 12 meses e alguns dias, como 360, 363 ou 364. 
+Este é apenas um exercício com objetivo de testar raciocínio matemático simples.*/
 #include <stdio.h>
- 
+
 int main() {
-    
+    int idade_dias, anos, meses, dias;
+
+    scanf("%d", &idade_dias);
+
+    anos = idade_dias / 365;
+    idade_dias = idade_dias % 365;
+
+    meses = idade_dias / 30;
+
+    dias = idade_dias % 30;
+
+    printf("%d ano(s)\n", anos);
+    printf("%d mes(es)\n", meses);
+    printf("%d dia(s)\n", dias);
+
     return 0;
 }
